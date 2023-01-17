a1f11bdb23d84815d25555e28ae27509fd394f2a
Herby <herbyosbourne31@gmail.com>
2023-01-17 14:33:06 -0500

    fix error

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const request = require("request");
-const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
@@ -10,59 +9,70 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.post("/reportjson", (req, res) => {
-  const { xKey, xCommand, xBeginDate, xEndDate } = req.body;
+  const {  xCommand, xBeginDate, xEndDate } = req.body;
+
+   const xkey ='johndeve1a162f74a86474bbf30ae6f3a989de8'
   const xVersion = "5.0.0";
   const xSoftwareName = "Cardknox";
   const xSoftwareVersion = "2.1";
 
-  if (!xKey || !xCommand || !xBeginDate || !xEndDate) {
+  if ( !xCommand || !xBeginDate || !xEndDate) {
     return res.status(400).json({ message: "Missing required parameters" });
   }
 
-  const options = {
-    url: "https://x1.cardknox.com/reportjson",
-    method: "POST",
-    headers: {
-      xKey: xKey,
-      xCommand: xCommand,
-      xBeginDate: xBeginDate,
-      xEndDate: xEndDate,
-      xVersion: xVersion,
-      xSoftwareName: xSoftwareName,
-      xSoftwareVersion: xSoftwareVersion,
-    },
-  };
-  // console.log(options);
+  // const options = {
+  //   url: "https://x1.cardknox.com/reportjson",
+  //   method: "POST",
+  //   headers: {
+  //     xKey: 'johndeve1a162f74a86474bbf30ae6f3a989de8',
+  //     // xKey: xKey,
+  //     xCommand: xCommand,
+  //     xBeginDate: xBeginDate,
+  //     xEndDate: xEndDate,
+  //     xVersion: xVersion,
+  //     xSoftwareName: xSoftwareName,
+  //     xSoftwareVersion: xSoftwareVersion,
+  //   },
+  // };
 
-  request(options, (error, response, body) => {
-    // if (error) {
-    //   return console.log(error);
-    // }
+  const body = JSON.stringify({
+    xKey: 'johndeve1a162f74a86474bbf30ae6f3a989de8',
+    xCommand: xCommand,
+    xVersion: xVersion,
+    xSoftwareName: xSoftwareName,
+    xSoftwareVersion: xSoftwareVersion,
+    xBeginDate: xBeginDate,
+    xEndDate: xEndDate,
+  });
+
+
+  request({url:'https://x1.cardknox.com/reportjson',method:'POST',body:body}, (error, response, body) => {
+    console.log({body})
     try {
       // Parse the response from the API
       let json = JSON.parse(body);
 
       // Extract the desired JSON object from the response
-      let result = {
-        xRefNum: json.xRefNum,
-        xCommand: json.xCommand,
-        xName: json.xName,
-        xMaskedCardNumber: json.xMaskedCardNumber,
-        xToken: json.xToken,
-        xAmount: json.xAmount,
-        xRequestAmount: json.xRequestAmount,
-        xCustom01: json.xCustom01,
-        xCustom02: json.xCustom02,
-        xEnteredDate: json.xEnteredDate,
-        xResponseAuthCode: json.xResponseAuthCode,
-        xResponseResult: json.xResponseResult,
-      };
+      // let result = {
+      //   xRefNum: json.xRefNum,
+      //   xCommand: json.xCommand,
+      //   xName: json.xName,
+      //   xMaskedCardNumber: json.xMaskedCardNumber,
+      //   xToken: json.xToken,
+      //   xAmount: json.xAmount,
+      //   xRequestAmount: json.xRequestAmount,
+      //   xCustom01: json.xCustom01,
+      //   xCustom02: json.xCustom02,
+      //   xEnteredDate: json.xEnteredDate,
+      //   xResponseAuthCode: json.xResponseAuthCode,
+      //   xResponseResult: json.xResponseResult,
+      // };
 
       // Send the desired JSON object as the response
-      console.log(result);
-      return res.status(200).json(result);
-      // console.log(result);
+      return res.status(200).json(body);
+
     } catch (e) {
+      console.log(e)
       return console.log("The response is not a valid JSON", body);
     }
   });
